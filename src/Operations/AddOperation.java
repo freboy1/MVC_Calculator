9ce141016195a281f10260e886c55d277f39c88a
Freboy1 <dautovalisher33@gmail.com>
2024-10-24 23:31:27 +0500

    Create AddOperation

---
+++ src/Operations/AddOperation.java
@@ -0,0 +1,8 @@
+package Operations;
+
+
+public class AddOperation implements Operation {
+    public double execute(double a, double b) {
+        return a + b;
+    }
+}
--- src/Operations/Operation.java
+++ src/Operations/Operation.java
@@ -1,3 +1,5 @@
+package Operations;
+
 public interface Operation {
     double execute(double a, double b);
 }
